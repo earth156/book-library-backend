af33735ee4b91787bc55470a54c94465cbff4097
earth156 <162809249+earth156@users.noreply.github.com>
2026-08-22 23:35:58 +0700

    "feat(server): register book and auth routes"

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 const categoryRoutes = require('./src/categories/routes/categoryRoutes');
 const authorRoutes = require('./src/authors/routes/authorRoutes');
+const bookRoutes = require('./src/books/routes/bookRoutes');
+const authRoutes = require('./src/auth/routes/authRoutes');
+
 // Middlewares พื้นฐาน
 app.use(cors()); // อนุญาตให้ Frontend เรียก API ข้ามพอร์ตได้
 app.use(express.json()); // อนุญาตให้รับข้อมูลแบบ JSON
@@ -16,15 +19,17 @@ app.use(express.json()); // อนุญาตให้รับข้อมู�
 // ใช้งาน Routes
 app.use('/api/categories', categoryRoutes);
 app.use('/api/authors', authorRoutes);
+app.use('/api/books', bookRoutes);
+
+app.use('/api', authRoutes);
 
 
-
-// Route พื้นฐานสำหรับทดสอบว่า Server ทำงานปกติ
 app.get('/', (req, res) => {
     res.send('Book library server is up and ready to roll');
 });
 
-// กำหนดให้ Server เริ่มทำงาน
+
 app.listen(PORT, () => {
+    console.log('Book library server is up and ready to roll');
     console.log(`Server is running on http://localhost:${PORT}`);
 });
